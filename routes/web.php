2e16e89d1865bebe47391cb1b1cecf513edca260
saiful63 <saifulislamraihan64@gmail.com>
2023-09-28 20:34:41 +0600

    Added

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[AuthController::class,'register_view'])->name('register');
 
-//Route::group(['middleware'=>'guest'],function(){
+Route::group(['middleware'=>'guest'],function(){
     Route::get('login',[AuthController::class,'index'])->name('login');
     Route::post('login',[AuthController::class,'login'])->name('login')->middleware('throttle:2,1');
     Route::post('register',[AuthController::class,'register'])->name('register')->middleware('throttle:2,1');
 
-//});
+});
 
-//Route::group(['middleware'=>'auth'],function(){
+Route::group(['middleware'=>'auth'],function(){
     Route::get('home',[AuthController::class,'home'])->name('home');
     Route::get('logout',[AuthController::class,'logout'])->name('logout');
 
     Route::get('{code}',[UrlShrotnerController::class,'getShrotenOne'])->name('getShrotenOne');
 
     Route::resource('url_shrotner',UrlShrotnerController::class);
-    
-//});
+
+});
 
 
