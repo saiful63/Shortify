5fdd51aa39b0a4e6bfa5137ad4d561980d9bee04
saiful63 <saifulislamraihan64@gmail.com>
2023-09-28 20:45:21 +0600

    Added

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[AuthController::class,'register_view'])->name('register');
-
-Route::group(['middleware'=>'guest'],function(){
-    Route::get('login',[AuthController::class,'index'])->name('login');
-    Route::post('login',[AuthController::class,'login'])->name('login')->middleware('throttle:2,1');
-    Route::post('register',[AuthController::class,'register'])->name('register')->middleware('throttle:2,1');
-
-});
+Route::get('login',[AuthController::class,'index'])->name('login');
+Route::post('login',[AuthController::class,'login'])->name('login')->middleware('throttle:2,1');
+Route::post('register',[AuthController::class,'register'])->name('register')->middleware('throttle:2,1');
 
 Route::group(['middleware'=>'auth'],function(){
     Route::get('home',[AuthController::class,'home'])->name('home');
